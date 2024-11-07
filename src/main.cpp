e04cb12f4b648a40b8b7d773dbc32baa0a566435
Andrew Chan <andrewkchan.akc@gmail.com>
2024-11-06 23:26:03 -0800

    add '-n' cli arg, load model and sample N steps

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 #include "codec.h"
+#include "model.h"
+#include "sampler.h"
 #include "tokenizer.h"
 
 void error_usage() {
@@ -9,6 +11,7 @@ void error_usage() {
 	fprintf(stderr, "Example: run model.yalm -i \"Q: What is the meaning of life?\"\n");
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, "  -i <string> input prompt\n");
+  fprintf(stderr, "  -n <int>    number of steps to run for, default 256. 0 = max_seq_len, -1 = infinite\n");
 	exit(1);
 }
 
@@ -16,6 +19,7 @@ int main(int argc, char* argv[]) {
   // default params
   std::string checkpoint_path = "";    // e.g. out/model.bin
   std::string prompt = "";             // prompt string
+  int num_steps = 256;                 // number of steps to run for
 
 	if (argc >= 2) {
 		checkpoint_path = argv[1];
@@ -41,7 +45,13 @@ int main(int argc, char* argv[]) {
       }
       prompt = argv[i + 1];
       i += 2;
-		} else {
+		} else if (argv[i][1] == 'n') {
+      if (i + 1 >= argc) {
+        error_usage();
+      }
+      num_steps = std::stoi(argv[i + 1]);
+      i += 2;
+    } else {
 			error_usage();
 		}
 	}
@@ -52,6 +62,9 @@ int main(int argc, char* argv[]) {
 
   YALMData model_data;
   model_data.from_file(checkpoint_path);
+  Model model(model_data);
+  InferenceState state(model.config);
+  Sampler sampler(model.config);
   Tokenizer tokenizer(model_data);
 
   std::vector<int> encoding = tokenizer.encode(prompt, true);
@@ -66,6 +79,23 @@ int main(int argc, char* argv[]) {
     }
   }
   std::cout << token_encoding_debug_str << std::endl;
+
+  // Hydrate KV cache by forwarding model on all prompt tokens and discarding output.
+  // This also generates output logits for the last token.
+  for (int pos = 0; pos < encoding.size(); pos++) {
+    int token_id = encoding[pos];
+    forward(state, model, token_id, pos);
+  }
+  // For N steps:
+  // - Sample + decode output logits
+  // - Forward the model
+  for (int i = 0; i < num_steps; i++) {
+    int token_id = sampler.sample_argmax(state.logits);
+    std::string token_str = tokenizer.decode_one(encoding.back(), token_id);
+    std::cout << token_str;
+    encoding.push_back(token_id);
+    forward(state, model, token_id, encoding.size() - 1);
+  }
 
   return 0;
 }
