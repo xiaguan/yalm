2a65dcbdff106976aeff1f08a037c6b6ece5b80b
Andrew Chan <andrewkchan.akc@gmail.com>
2024-11-13 00:54:44 -0800

    parallelize matmul via openmp

--- src/infer.cpp
+++ src/infer.cpp
@@ -7,6 +7,7 @@
 static void matmul(float* xout, float* x, float* w, int n, int d) {
 	// W (d,n) @ x (n,) -> xout (d,)
 	int i;
+#pragma omp parallel for private(i)
 	for (i = 0; i < d; i++) {
 		float val = 0.0f;
 		for (int j = 0; j < n; j++) {
